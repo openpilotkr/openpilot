bec85da0e1f6fb8b021f3511e986640e4f6fafe6
multikyd <72329880+multikyd@users.noreply.github.com>
2023-02-02 01:38:50 +0900

    opkr

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -628,18 +628,15 @@ class Controls:
           self.osm_waze_off_spdlimit = True
         elif osm_waze_speedlimit != self.v_cruise_kph:
           if self.map_enabled and self.navi_selection == 3 and self.sm['liveNaviData'].wazeRoadSpeedLimit > 21:
-            self.osm_waze_speedlimit = 255
-            self.osm_waze_off_spdlimit_init = False
+            self.osm_waze_off_spdlimit_init = True
             self.v_cruise_kph = osm_waze_speedlimit
             self.v_cruise_kph_last = self.v_cruise_kph
           elif self.navi_selection == 5 and self.sm['liveENaviData'].wazeRoadSpeedLimit > 21:
-            self.osm_waze_speedlimit = 255
-            self.osm_waze_off_spdlimit_init = False
+            self.osm_waze_off_spdlimit_init = True
             self.v_cruise_kph = osm_waze_speedlimit
             self.v_cruise_kph_last = self.v_cruise_kph
           elif self.osm_speedlimit_enabled and self.sm['liveMapData'].speedLimit > 21:
-            self.osm_waze_speedlimit = 255
-            self.osm_waze_off_spdlimit_init = False
+            self.osm_waze_off_spdlimit_init = True
             self.v_cruise_kph = osm_waze_speedlimit
             self.v_cruise_kph_last = self.v_cruise_kph
 
