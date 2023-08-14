40f9b6e1bbc7245df33546aa5cacb0d696e868cf
multikyd <72329880+multikyd@users.noreply.github.com>
2023-08-14 12:59:17 +0900

    fix no lat

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -737,7 +737,7 @@ class Controls:
       #print('active={}  steerFaultTemporary={}  steerFaultPermanent={}  standstill={}  joystick_mode={}  lkas_temporary_off={}'.format(
       # self.active, CS.steerFaultTemporary, CS.steerFaultPermanent, standstill, self.joystick_mode, self.lkas_temporary_off))
     else:
-      standstill = CS.vEgo <= max(self.CP.minSteerSpeed, MIN_LATERAL_CONTROL_SPEED) or CS.standstill
+      standstill = (CS.vEgo <= max(self.CP.minSteerSpeed, MIN_LATERAL_CONTROL_SPEED) and self.no_mdps_mods) or CS.standstill
       CC.latActive = self.active and not CS.steerFaultTemporary and not CS.steerFaultPermanent and \
                      (not standstill or self.joystick_mode)
       CC.longActive = self.enabled and not self.events.any(ET.OVERRIDE_LONGITUDINAL) and self.CP.openpilotLongitudinalControl
