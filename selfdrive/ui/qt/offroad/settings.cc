0805fbc930ca5438976052f98a47ee13fa87c345
multikyd <multikyd@gmail.com>
2023-08-22 00:46:51 +0900

    missing ,

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -60,7 +60,7 @@ TogglesPanel::TogglesPanel(SettingsWindow *parent) : ListWidget(parent) {
                         "These mistakes can include unintended laneline crossings, late exit taking, driving towards dividing barriers in the gore areas, etc."))
       .arg(tr("New Driving Visualization"))
       .arg(tr("The driving visualization will transition to the road-facing wide-angle camera at low speeds to better show some turns. The Experimental mode logo will also be shown in the top right corner. "
-              "When a navigation destination is set and the driving model is using it as input, the driving path on the map will turn green."));
+              "When a navigation destination is set and the driving model is using it as input, the driving path on the map will turn green.")),
       "../assets/img_experimental_white.svg",
     },
     {
