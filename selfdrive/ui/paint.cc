f563ad259153f36bef3cb37b6fd79f60a8fa1962
multikyd <multikyd@gmail.com>
2023-09-26 23:46:30 +0900

    fixed?

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1231,7 +1231,7 @@ static void bb_ui_draw_measures_left(UIState *s, int bb_x, int bb_y, int bb_w ) 
     char uom_str[6];
     NVGcolor val_color = COLOR_WHITE_ALPHA(200);
     if (scene.controls_state.getEnabled()) {
-      snprintf(val_str, sizeof(val_str), "%.0f",(scene.steer_max*scene.output_scale));
+      snprintf(val_str, sizeof(val_str), "%.0f",((float)scene.steer_max*(float)scene.output_scale));
     } else {
        snprintf(val_str, sizeof(val_str), "-");
     }
