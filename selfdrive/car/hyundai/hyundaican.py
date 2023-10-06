12ad6819c2d0ba8620fd5c79445c5396353d9acc
multikyd <72329880+multikyd@users.noreply.github.com>
2023-10-06 23:12:26 +0900

    no lat test

--- selfdrive/car/hyundai/hyundaican.py
+++ selfdrive/car/hyundai/hyundaican.py
@@ -1,5 +1,5 @@
 import crcmod
-from openpilot.selfdrive.car.hyundai.values import CAR, CHECKSUM, CAMERA_SCC_CAR, HyundaiFlags, LEGACY_SAFETY_MODE_CAR_ALT
+from openpilot.selfdrive.car.hyundai.values import CAR, CHECKSUM, CAMERA_SCC_CAR, HyundaiFlags, LEGACY_SAFETY_MODE_CAR_ALT, LEGACY_SAFETY_MODE_CAR_ALT2
 
 hyundai_checksum = crcmod.mkCrcFun(0x11D, initCrc=0xFD, rev=False, xorOut=0xdf)
 
@@ -25,11 +25,11 @@ def create_lkas11(packer, frame, car_fingerprint, apply_steer, steer_req,
     "CF_Lkas_LdwsOpt_USM",
   ]}
   values["CF_Lkas_LdwsSysState"] = sys_state
-  values["CF_Lkas_SysWarning"] = 3 if sys_warning else 0 # if (sys_warning and car_fingerprint not in LEGACY_SAFETY_MODE_CAR_ALT) else 0
+  values["CF_Lkas_SysWarning"] = 3 if sys_warning else 0
   values["CF_Lkas_LdwsLHWarning"] = left_lane_depart
   values["CF_Lkas_LdwsRHWarning"] = right_lane_depart
   values["CR_Lkas_StrToqReq"] = apply_steer
-  values["CF_Lkas_ActToi"] = steer_req # and not (torque_fault and (1 if car_fingerprint in LEGACY_SAFETY_MODE_CAR_ALT else 0))
+  values["CF_Lkas_ActToi"] = steer_req and not (torque_fault and (True if car_fingerprint in LEGACY_SAFETY_MODE_CAR_ALT2 else False))
   values["CF_Lkas_ToiFlt"] = torque_fault  # seems to allow actuation on CR_Lkas_StrToqReq
   values["CF_Lkas_MsgCount"] = frame % 0x10
 
